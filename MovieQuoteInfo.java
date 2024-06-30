e17bbe334ef3fa8d2a7063788b4b035646c7dd14
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-06-30 07:33:30 +0200

    Add files via upload

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+ public static void main(String[] args)
+ {
+  System.out.println("You got the power to have it all, but too weak to take it");
+  System.out.println("Green Goblin");
+  System.out.println("Spiderman No Way Home");
+  System.out.println("2021");
+ }
+}
