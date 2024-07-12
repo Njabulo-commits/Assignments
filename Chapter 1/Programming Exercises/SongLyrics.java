eb2ba4e284ad67a953109f57bb6b118d13e6c6d0
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-07-12 14:12:57 +0200

    Add files via upload

---
+++ Chapter 1/Programming Exercises/SongLyrics.java
@@ -0,0 +1,10 @@
+public class SongLyrics
+{
+ public static void main(String[] args)
+ {
+  System.out.println("Close I stare at my dreams as they approach");
+  System.out.println("Gotta be patient and trust in God he the coach");
+  System.out.println("Temptations be taking a shortcut but I don't");
+  System.out.println("Coz I aint tryna be an almost when I get it I'mma float");
+ }
+}
