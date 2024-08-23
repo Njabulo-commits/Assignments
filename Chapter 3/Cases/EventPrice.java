e1e17099c1426c1d264fcb8020852d415b2c0771
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-08-23 15:00:18 +0200

    Update EventPrice.java

--- Chapter 3/Cases/EventPrice.java
+++ Chapter 3/Cases/EventPrice.java
@@ -1,3 +1,16 @@
+/*
+Yummy Catering provides meals for parties and special events. In Chapter 2, you wrote an application that 
+prompts the user for the number of guests attending an event, displays the company motto with a border, and 
+then displays the price of the event and whether the event is a large one. Now modify the program so that the 
+main() method contains only three executable statements that each call a method as follows:
+❯ The first executable statement calls a public static int method that prompts the user for the 
+number of guests and returns the value to the main() method.
+❯ The second executable statement calls a public static void method that displays the company 
+motto with the border.
+❯ The last executable statement passes the number of guests to a public static void method that 
+computes the price of the event, displays the price, and displays whether the event is a large event.
+Save the file as YummyEventPriceWithMethods.java
+*/
 import java.util.Scanner;
 
 public class EventPrice {
