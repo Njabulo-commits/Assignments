87b590b99aa008b0ae8bbabcea9cddddb169638f
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-08-23 14:54:57 +0200

    Update MadLibWithMethod.java

--- Chapter 3/Game Zone/MadLibWithMethod.java
+++ Chapter 3/Game Zone/MadLibWithMethod.java
@@ -1,3 +1,15 @@
+/*
+Mad Libs is a children’s game in which the players provide a few words that are then incorporated into a silly 
+story. For example, you might ask a child for the name of an animal, a number, and a direction. The child might 
+reply with such answers as dog, 13, and east. The newly created Mad Lib might be:
+Hickory Dickory Dock.
+The dog ran up the clock.
+The clock struck 13.
+The dog ran east.
+Hickory Dickory Dock.
+Create a program that asks the user to provide a few words. Then send the words to a method that displays a 
+short story or nursery rhyme that uses them. Save the file as MadLibWithMethod.java.
+*/
 import java.util.Scanner;
 public class MadLibWithMethod {
     static Scanner input = new Scanner(System.in);
