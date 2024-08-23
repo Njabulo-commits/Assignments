8ac2212b1edeb2fb776ebed382e573e6e8edf139
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-08-23 15:01:58 +0200

    Update SunshineRentalPrice.java

--- Chapter 3/Cases/SunshineRentalPrice.java
+++ Chapter 3/Cases/SunshineRentalPrice.java
@@ -1,3 +1,16 @@
+/*
+Sunshine Seashore Supplies rents beach equipment such as kayaks, canoes, beach chairs, and umbrellas to 
+tourists. In Chapter 2, you wrote an application that prompts the user for the number of minutes a piece of 
+sports equipment was rented, displays the company motto with a border, and displays the price for the rental. 
+Now modify the program so that the main() method contains only three executable statements that each call 
+a method as follows:
+❯ The first executable statement calls a method that prompts the user for the rental time in minutes and 
+returns the value to the main() method.
+❯ The second executable statement calls a method that displays the company motto with the border.
+❯ The last executable statement passes the number of minutes to a method that computes the hours, extra 
+minutes, and price for the rental and then displays all the details.
+Save the file as SunshineRentalPriceWithMethods.java
+*/
 import java.util.Scanner;
 public class SunshineRentalPrice {
     private static final float RENTAL_COST_PER_HOUR = 40.00F;
