940c8eb4f5c6d285a56f71b088b9e5a08a2d5026
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-10-15 23:55:02 +0200

    Update booking_validation.js

--- Handy-Max-Website/booking_validation.js
+++ Handy-Max-Website/booking_validation.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById('booking_form');
 const firstName = document.getElementById('first_name');
 const lastName = document.getElementById('last_name');
 const email = document.getElementById('email');
@@ -7,9 +7,12 @@ const services = document.getElementById('services');
 
 
 form.addEventListener('submit', e => {
-	e.preventDefault();
-	
-	checkInputs();
+
+	if (checkInputs()) { 
+        form.submit();  
+    }else{
+        e.preventDefault();
+    }
 });
 
 function checkInputs() {
@@ -19,6 +22,7 @@ function checkInputs() {
 	const emailValue = email.value.trim();
     const serviceValue = services.value;
     const dateValue = date.value;
+    
     // Get the value of the date input and convert it to a Date object
     const selectedDate = new Date(date.value);
 
@@ -28,19 +32,23 @@ function checkInputs() {
 	
 	if(firstNameValue === '') {
 		setErrorFor(firstName, 'First Name cannot be blank');
+		isValid = false;
 	} else {
 		setSuccessFor(firstName);
 	}
 	if(lastNameValue === '') {
 		setErrorFor(lastName, 'Surname cannot be blank');
+		isValid = false;
 	} else {
 		setSuccessFor(lastName);
 	}
 	
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
+		isValid = false;
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
+		isValid = false;
 	} else {
 		setSuccessFor(email);
 	}
@@ -48,6 +56,7 @@ function checkInputs() {
     // Check if value was selected from the dropdown
     if(serviceValue === '') {
         setErrorFor(services, 'Please select a service.');
+		isValid = false;
     } else {
         setSuccessFor(services);
     }
@@ -55,12 +64,15 @@ function checkInputs() {
     // Check if the selected date is in the past
     if (dateValue === '') {
         setErrorFor(date, 'Please select a date.');
+		isValid = false;
     } else if (selectedDate < today) {
         setErrorFor(date, 'The appointment date cannot be in the past.');
+		isValid = false;
     } else {
         // Clear any existing error messages if the date is valid
         setSuccessFor(date);
     }
+	return isValid; // Return whether the form is valid or not
 }
 
 function setErrorFor(input, message) {
