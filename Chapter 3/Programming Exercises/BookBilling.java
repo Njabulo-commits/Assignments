98758ea6071ec2d4bd522cfac9282ad57430ba8c
Njabulo-commits <155539968+Njabulo-commits@users.noreply.github.com>
2024-11-26 12:41:14 +0200

    Update BookBilling.java

--- Chapter 3/Programming Exercises/BookBilling.java
+++ Chapter 3/Programming Exercises/BookBilling.java
@@ -1,3 +1,15 @@
+/*
+    1. Create a class named `BookBilling` that includes three overloaded `computeBill()` methods for the Happy Memories Company, which sells photo books.
+    
+        ❯ When `computeBill()` receives no parameters, the method computes the price of one photo book at $14.99, adds 8 percent tax, and returns the total due.
+    
+        ❯ When `computeBill()` receives one parameter, it represents the quantity ordered. Multiply the value by $14.99, add 8 percent tax, and return the total due.
+    
+        ❯ When `computeBill()` receives two parameters, they represent the quantity ordered and a coupon value. Multiply the quantity by $14.99, reduce the result by the coupon value, add 8 percent tax, and then return the total due. 
+    
+    Write a main() method that prompts the user for the number of books ordered, prompts for a coupon value, and tests all three overloaded methods. Save the application as **`BookBilling.java`**.
+*/
+
 import java.util.Scanner;
 
 public class BookBilling {
@@ -33,6 +45,7 @@ public class BookBilling {
         int numBooks;
         double couponValue;
         Scanner input = new Scanner(System.in);
+        
         System.out.println("Enter the number of books ordered>> ");
         numBooks = input.nextInt();
 
